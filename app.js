cac7bb6599a3b7cfb9c20073d17eec10b4a2eb18
WhiteAdy <adi.tamimi@live.com>
2019-08-11 03:28:39 +0300

    meh

--- app.js
+++ app.js
@@ -8,31 +8,31 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-mongoose.connect(
-	'mongodb+srv://Mozartino:Mozarella920%23@mozzarella-0ndgi.azure.mongodb.net/meh?retryWrites=true&w=majority',
-	{ useNewUrlParser: true }
-);
-let db = mongoose.connection;
+app.get('/mongo', (req, res) => {
+	mongoose.connect(
+		'mongodb+srv://Mozartino:Mozarella920%23@mozzarella-0ndgi.azure.mongodb.net/meh?retryWrites=true&w=majority',
+		{ useNewUrlParser: true }
+	);
+	let db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-	// let mozzarellaSchema = new Schema(
-	// 	{
-	// 		name: String,
-	// 		email: String,
-	// 		movie_id: ObjectId,
-	// 		text: String,
-	// 		date: { type: Date, default: Date.now }
-	// 	},
-	// 	{
-	// 		collection: 'Mozzarella'
-	// 	}
-	// );
-	// let Mozzarella = mongoose.model('Mozzarella', mozzarellaSchema);
-	db.db.collection('meh_collection', function(err, collection) {
-		collection.find().toArray(function(err, data) {
-			// console.log('data: ', data); // it will print your collection data
-			app.get('/mongo', (req, res) => {
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function() {
+		// let mozzarellaSchema = new Schema(
+		// 	{
+		// 		name: String,
+		// 		email: String,
+		// 		movie_id: ObjectId,
+		// 		text: String,
+		// 		date: { type: Date, default: Date.now }
+		// 	},
+		// 	{
+		// 		collection: 'Mozzarella'
+		// 	}
+		// );
+		// let Mozzarella = mongoose.model('Mozzarella', mozzarellaSchema);
+		db.db.collection('meh_collection', function(err, collection) {
+			collection.find().toArray(function(err, data) {
+				// console.log('data: ', data); // it will print your collection data
 				res.json({ caca: data });
 			});
 		});
